817a68a94b9561f1d625ec50951b504c1f4887e6
g21660ar <adan.raheel@student.manchester.ac.uk>
2020-08-29 21:27:42 +0100

    new

--- Routes/GraphDriver.java
+++ Routes/GraphDriver.java
@@ -6,6 +6,7 @@
  * @version (a version number or a date)
  */
 import java.util.Scanner;
+import java.io.Console;
 import java.util.ArrayList;
 import javafx.application.Application;
 
@@ -13,22 +14,26 @@ public class GraphDriver
 {
     static int MAX_ROW, MAX_COL;
     public static void main(String[] params) {
-        Scanner input = new Scanner(System.in);
+        start();
+    }
+    public static void start() {
+        Console input = System.console();
+        //Scanner input = new Scanner(System.in);
         System.out.print("Enter max vertical coordinate: ");
-        MAX_ROW = input.nextInt();
+        MAX_ROW = Integer.parseInt(input.readLine());
         System.out.print("Enter max horizontal coordinate: ");
-        MAX_COL = input.nextInt();
+        MAX_COL = Integer.parseInt(input.readLine());
         System.out.println("\nEnter node info below in the following format: ");
         System.out.println("[NODE SYMBOL],[ROW],[COLUMN]");
         System.out.println("Enter 'EOL' to finish node initialisation. \n");
-        String node_info = "";
+        String node_info = "AAAAA";
         int node_count = 0;
         
-        while (node_info != "EOL") {
+        while (!node_info.equals("EOL")) {
             node_count += 1;
             System.out.println("Node "+node_count+": ");
-            node_info = input.nextLine();
-            if (node_info != "EOL") {
+            node_info = (input.readLine());
+            if (!node_info.equals("EOL")) {
                 Node n = new Node(node_info.charAt(0), node_info.charAt(2), node_info.charAt(4));
                 Graph.addNode(n); // Requires input validation
             }
@@ -38,14 +43,15 @@ public class GraphDriver
         System.out.println("\nEnter node links below in the following format: ");
         System.out.println("[NODE 1],[NODE 2]");
         System.out.println("Enter 'EOL' to finish node link input. \n");
-        while (node_linkage != "EOL") {
+        while (!node_linkage.equals("EOL")) {
             System.out.println("Enter link: ");
-            node_info = input.nextLine();
-            if (node_linkage != "EOL") {
-                Node n1 = Node.getNode(node_info.charAt(0));
-                Node n2 = Node.getNode(node_info.charAt(2));
+            node_linkage = (input.readLine());
+            if (!node_linkage.equals("EOL")) {
+                Node n1 = Node.getNode(node_linkage.charAt(0));
+                Node n2 = Node.getNode(node_linkage.charAt(2));
                 n1.addLink(n2);
             }
         }
+        //Application.launch(Visualise.class, params);
     }
 }
--- Routes/Visualise.java
+++ Routes/Visualise.java
@@ -10,6 +10,8 @@ import javafx.scene.control.Label;
 import javafx.scene.shape.Line;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import java.io.Console;
+import java.util.Scanner;
 
 /**
  * Write a description of JavaFX class Visualise here.
@@ -30,9 +32,51 @@ public class Visualise extends Application
      *
      * @param  stage the primary stage for this application.
      */
+    static int MAX_ROW, MAX_COL;
     @Override
     public void start(Stage stage)
     {
+       // GraphDriver.start();
+        Scanner sin = new Scanner(System.in);
+        
+        Console input = System.console();
+        //Scanner input = new Scanner(System.in);
+        System.out.print("Enter max vertical coordinate: ");
+        MAX_ROW = (sin.nextInt());
+        //MAX_ROW = Integer.parseInt(input.readLine());
+        System.out.print("Enter max horizontal coordinate: ");
+        MAX_COL = Integer.parseInt(input.readLine());
+        System.out.println("\nEnter node info below in the following format: ");
+        System.out.println("[NODE SYMBOL],[ROW],[COLUMN]");
+        System.out.println("Enter 'EOL' to finish node initialisation. \n");
+        String node_info = "AAAAA";
+        int node_count = 0;
+        
+        while (!node_info.equals("EOL")) {
+            node_count += 1;
+            System.out.println("Node "+node_count+": ");
+            node_info = (input.readLine());
+            if (!node_info.equals("EOL")) {
+                Node n = new Node(node_info.charAt(0), node_info.charAt(2), node_info.charAt(4));
+                Graph.addNode(n); // Requires input validation
+            }
+        }
+        
+        String node_linkage = "";
+        System.out.println("\nEnter node links below in the following format: ");
+        System.out.println("[NODE 1],[NODE 2]");
+        System.out.println("Enter 'EOL' to finish node link input. \n");
+        while (!node_linkage.equals("EOL")) {
+            System.out.println("Enter link: ");
+            node_linkage = (input.readLine());
+            if (!node_linkage.equals("EOL")) {
+                Node n1 = Node.getNode(node_linkage.charAt(0));
+                Node n2 = Node.getNode(node_linkage.charAt(2));
+                n1.addLink(n2);
+            }
+        }
+        //Application.launch(Visualise.class, params);
+        
         // Create a Button or any control item
         Button myButton = new Button("Count");
 
