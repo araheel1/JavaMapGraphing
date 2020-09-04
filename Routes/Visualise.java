7ce4001140a931e63bad005fc943f4df8a9ece78
g21660ar <adan.raheel@student.manchester.ac.uk>
2020-09-04 09:20:40 -0700

    Visualisation polish

--- Routes/Visualise.java
+++ Routes/Visualise.java
@@ -6,7 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
+import javafx.scene.text.*;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Circle;
 import javafx.scene.layout.GridPane;
@@ -25,87 +25,94 @@ import java.util.ArrayList;
  */
 public class Visualise extends Application
 {
-    // We keep track of the count, and label displaying the count:
-    private int count = 0;
-    //private Label myLabel = new Label("0");
+    static int MAX_ROW, MAX_COL;
+
+    public void get_info() {
+      // GraphDriver.start();
+      // Scanner sin = new Scanner(System.in);
+       Console input = System.console();
+       //Scanner input = new Scanner(System.in);
+
+
+       System.out.print("Enter max vertical coordinate: ");
+       MAX_ROW = Integer.parseInt(input.readLine());
+       //MAX_ROW = Integer.parseInt(input.readLine());
+       System.out.print("Enter max horizontal coordinate: ");
+       MAX_COL = Integer.parseInt(input.readLine());
+       System.out.println("\nEnter node info below in the following format: ");
+       System.out.println("[NODE SYMBOL],[ROW],[COLUMN]");
+       System.out.println("Enter 'EOL' to finish node initialisation. \n");
+       String node_info = "AAAAA";
+       int node_count = 0;
+
+       while (!node_info.equals("EOL")) {
+           node_count += 1;
+           System.out.println("Node "+node_count+": ");
+           node_info = (input.readLine());
+           if (!node_info.equals("EOL")) {
+               Node n = new Node(node_info.charAt(0), Character.digit(node_info.charAt(2), 10),
+               Character.digit(node_info.charAt(4), 10));
+               Graph.addNode(n); // Requires input validation
+           }
+       }
+
+       String node_linkage = "";
+       System.out.println("\nEnter node links below in the following format: ");
+       System.out.println("[NODE 1],[NODE 2]");
+       System.out.println("Enter 'EOL' to finish node link input. \n");
+       while (!node_linkage.equals("EOL")) {
+           System.out.println("Enter link: ");
+           node_linkage = (input.readLine());
+           if (!node_linkage.equals("EOL")) {
+               Node n1 = Node.getNode(node_linkage.charAt(0));
+               Node n2 = Node.getNode(node_linkage.charAt(2));
+               n1.addLink(n2);
+           }
+       }
+    }
 
     /**
-     * The start method is the main entry point for every JavaFX application. 
-     * It is called after the init() method has returned and after 
+     * The start method is the main entry point for every JavaFX application.
+     * It is called after the init() method has returned and after
      * the system is ready for the application to begin running.
      *
      * @param  stage the primary stage for this application.
      */
-    static int MAX_ROW, MAX_COL;
     @Override
     public void start(Stage stage)
     {
-       // GraphDriver.start();
-       // Scanner sin = new Scanner(System.in);
-        
-        Console input = System.console();
-        //Scanner input = new Scanner(System.in);
-        System.out.print("Enter max vertical coordinate: ");
-        MAX_ROW = Integer.parseInt(input.readLine());
-        //MAX_ROW = Integer.parseInt(input.readLine());
-        System.out.print("Enter max horizontal coordinate: ");
-        MAX_COL = Integer.parseInt(input.readLine());
-        System.out.println("\nEnter node info below in the following format: ");
-        System.out.println("[NODE SYMBOL],[ROW],[COLUMN]");
-        System.out.println("Enter 'EOL' to finish node initialisation. \n");
-        String node_info = "AAAAA";
-        int node_count = 0;
-        
-        while (!node_info.equals("EOL")) {
-            node_count += 1;
-            System.out.println("Node "+node_count+": ");
-            node_info = (input.readLine());
-            if (!node_info.equals("EOL")) {
-                Node n = new Node(node_info.charAt(0), Character.digit(node_info.charAt(2), 10), Character.digit(node_info.charAt(4), 10));
-                Graph.addNode(n); // Requires input validation
-            }
-        }
-        
-        String node_linkage = "";
-        System.out.println("\nEnter node links below in the following format: ");
-        System.out.println("[NODE 1],[NODE 2]");
-        System.out.println("Enter 'EOL' to finish node link input. \n");
-        while (!node_linkage.equals("EOL")) {
-            System.out.println("Enter link: ");
-            node_linkage = (input.readLine());
-            if (!node_linkage.equals("EOL")) {
-                Node n1 = Node.getNode(node_linkage.charAt(0));
-                Node n2 = Node.getNode(node_linkage.charAt(2));
-                n1.addLink(n2);
-            }
-        }
+       get_info();
         //Application.launch(Visualise.class, params);
-        
+
         // Create a Button or any control item
-        Button myButton = new Button("Count");
+        //Button myButton = new Button("Count");
 
         // Create a new grid pane
         //GridPane pane = new GridPane();
-	Pane canvas = new Pane();
-	canvas.setPrefSize(600,600);
+	       Pane canvas = new Pane();
+	       canvas.setPrefSize(600,600);
         //pane.setPadding(new Insets(10, 10, 10, 10));
         //pane.setMinSize(750, 750);
         //pane.setVgap(10);
         //pane.setHgap(10);
 
         //set an action on the button using method reference
-        myButton.setOnAction(this::buttonClick);
+        //myButton.setOnAction(this::buttonClick);
 
         // Add the button and label into the pane
         for (Node n : Graph.getNodes()) {
-	    System.out.println(n.getRow()+" "+n.getCol()+" "+n.getSymbol());
-            canvas.getChildren().add(new Circle(50*(n.getRow()+1), 50*(n.getCol()+1), 25));
-            canvas.getChildren().add(new Text(50*(n.getRow()+1), 50*(n.getCol()+1)+100, String.valueOf(n.getSymbol())));
-	    ArrayList<Object[]> links = n.getLinks();
-	    for (int x=0; x<links.size(); x++) {
-	        Node n2 = (Node) links.get(x)[0];
-		canvas.getChildren().add(new Line(50*(n.getRow()+1), 50*(n.getCol()+1), 50*(n2.getRow()+1), 50*(n2.getCol()+1)));
-	    }
+	          System.out.println(n.getRow()+" "+n.getCol()+" "+n.getSymbol());
+            canvas.getChildren().add(new Circle(50*(n.getRow()+1), 50*(n.getCol()+1), 10));
+            Text temp_text = new Text(50*(n.getRow()+1)-10, 50*(n.getCol()+1)+40,
+            String.valueOf(n.getSymbol()));
+            temp_text.setFont(Font.font(30));
+            canvas.getChildren().add(temp_text);
+      	    ArrayList<Object[]> links = n.getLinks();
+      	    for (int x=0; x<links.size(); x++) {
+      	        Node n2 = (Node) links.get(x)[0];
+      		      canvas.getChildren().add(new Line(50*(n.getRow()+1), 50*(n.getCol()+1),
+                50*(n2.getRow()+1), 50*(n2.getCol()+1)));
+      	    }
         }
 	//pane.add(new Circle(10, Color.RED), 50, 50);
 	//canvas.getChildren().add(pane);
@@ -125,10 +132,4 @@ public class Visualise extends Application
      * This will be executed when the button is clicked
      * It increments the count by 1
      */
-    private void buttonClick(ActionEvent event)
-    {
-        // Counts number of button clicks and shows the result on a label
-        count = count + 1;
-        //myLabel.setText(Integer.toString(count));
-    }
 }
